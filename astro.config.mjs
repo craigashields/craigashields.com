e8864e09d3451542fcd84431360b9365e26a6c6b
craigashields <craig_a_shields@hotmail.co.uk>
2023-08-20 19:11:51 +0100

    fixed GA

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 import { defineConfig } from 'astro/config';
 import { remarkReadingTime } from './remark-reading-time.mjs';
+import partytown from "@astrojs/partytown";
 
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
   site: "https://craigashields.com",
-  integrations: [mdx()],
+  integrations: [mdx(),partytown({
+    // Adds dataLayer.push as a forwarding-event.
+    config: {
+      forward: ["dataLayer.push"],
+    },
+  })],
   markdown: {
     remarkPlugins: [remarkReadingTime]
   },
